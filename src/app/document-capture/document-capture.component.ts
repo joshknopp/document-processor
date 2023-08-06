4647b5f391869dcccbf4540a1b70ef3407cdcf07
joshknopp <github@joshknopp.com>
2023-08-06 03:28:20 +0000

    A bit of thresholding to assist with OCR

--- src/app/document-capture/document-capture.component.ts
+++ src/app/document-capture/document-capture.component.ts
@@ -57,8 +57,17 @@ export class DocumentCaptureComponent implements AfterViewInit {
 
   captureImage() {
     setTimeout(async () => {
-      this.canvas.getContext('2d').drawImage(this.video, 0, 0, this.video.videoWidth, this.video.videoHeight);
-      this.capturedImage = this.canvas.toDataURL('image/png');
+      const context = this.canvas.getContext('2d');
+      context.drawImage(this.video, 0, 0, this.video.videoWidth, this.video.videoHeight);
+
+      const imageData = context.getImageData(0, 0, this.video.videoWidth, this.video.videoHeight);
+
+      this.thresholdImage(imageData, 128);
+      
+
+      this.capturedImage = this.canvas.toDataURL();
+
+
       this.video.srcObject = null;
       this.video.load();
       // OCR text extraction
@@ -70,6 +79,61 @@ export class DocumentCaptureComponent implements AfterViewInit {
         console.error('OCR Error:', err);
       }
     }, 1);
+  }
+
+  thresholdImage(imageData: any, thresholdValue: number) {
+    if (imageData) {
+      const data = imageData.data;
+
+      // Calculate the histogram
+      const histogram = new Array(256).fill(0);
+      for (let i = 0; i < data.length; i += 4) {
+        const averageValue = (data[i] + data[i + 1] + data[i + 2]) / 3;
+        histogram[Math.floor(averageValue)]++;
+      }
+
+      // Calculate the total number of pixels
+      const totalPixels = imageData.width * imageData.height;
+      let sum = 0;
+      for (let i = 0; i < 256; i++) {
+        sum += i * histogram[i];
+      }
+
+      let sumB = 0;
+      let wB = 0;
+      let wF = 0;
+      let mB;
+      let mF;
+      let maxVariance = 0;
+      let threshold = 0;
+
+      for (let t = 0; t < 256; t++) {
+        wB += histogram[t];
+        if (wB === 0) continue;
+
+        wF = totalPixels - wB;
+        if (wF === 0) break;
+
+        sumB += t * histogram[t];
+        mB = sumB / wB;
+        mF = (sum - sumB) / wF;
+
+        const varianceBetween = wB * wF * Math.pow(mB - mF, 2);
+        if (varianceBetween > maxVariance) {
+          maxVariance = varianceBetween;
+          threshold = t;
+        }
+      }
+
+      for (let i = 0; i < data.length; i += 4) {
+        const averageValue = (data[i] + data[i + 1] + data[i + 2]) / 3;
+        const newValue = averageValue < threshold ? 0 : 255;
+        data[i] = data[i + 1] = data[i + 2] = newValue;
+      }
+
+      const context = this.canvas.getContext('2d');
+      context.putImageData(imageData, 0, 0);
+    }
   }
 
   async extractTextFromImage(image: Tesseract.ImageLike) {
